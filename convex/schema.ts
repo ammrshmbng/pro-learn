bb5fac35e967a9d77a5fa2b9fa3f0d98b1bf6a79
ammar sihombing <ammarmare01@gmail.com>
2025-04-27 18:00:50 +0700

    get data courses, and create component to display them

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,9 +2,18 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+
   users: defineTable({
     email: v.string(),
     name: v.string(),
     clerkId: v.string(),
   }).index("by_clerkId", ["clerkId"]),
+
+  courses: defineTable({
+		title: v.string(),
+		description: v.string(),
+		imageUrl: v.string(),
+		price: v.number(),
+	}),
+
 });
