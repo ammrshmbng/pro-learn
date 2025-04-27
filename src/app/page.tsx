eafead75565bec779edabd32a7f04b7ae7ee882a
ammar sihombing <ammarmare01@gmail.com>
2025-04-27 16:29:37 +0700

    create navbar component, and add login and logout function, and webhook to sync auth system between clerk and convex, and create http action, create schema,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,11 @@ import { api } from "../../convex/_generated/api";
 
 export default function Home() {
 
-  const tasks = useQuery(api.tasks.getAllTasks);
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">All Tasks in DB</h1>
-      {
-        tasks?.map((task)=>{
-          return (
-            <div key={task?.id}>
-                <h2>{task?.text} = is completed: {task?.isCompleted ? "true" : "false"}</h2>
-                
-            </div>
-          )
-        })
-      }
+      
     </>
   );
 }
