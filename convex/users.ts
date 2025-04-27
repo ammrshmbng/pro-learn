1b5588bd650dc00835c78447d3b85803ad56d720
ammar sihombing <ammarmare01@gmail.com>
2025-04-27 20:36:13 +0700

    create function to check user access by stripe customer id and clerk id and so forth

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
-import { mutation,} from "./_generated/server";
-import { v } from "convex/values";
+import { mutation,query} from "./_generated/server";
+import { ConvexError, v } from "convex/values";
 
 
 export const createUser = mutation({
@@ -28,5 +28,64 @@ export const createUser = mutation({
 		});
 
 		return userId;
+	},
+});
+
+export const getUserByClerkId = query({
+	args: { clerkId: v.string() },
+	handler: async (ctx, args) => {
+		return await ctx.db
+			.query("users")
+			.withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
+			.unique();
+	},
+});
+
+export const getUserByStripeCustomerId = query({
+	args: { stripeCustomerId: v.string() },
+	handler: async (ctx, args) => {
+		return await ctx.db
+			.query("users")
+			.withIndex("by_stripeCustomerId", (q) => q.eq("stripeCustomerId", args.stripeCustomerId))
+			.unique();
+	},
+});
+
+export const getUserAccess = query({
+	args: { userId: v.id("users"), courseId: v.id("courses") },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+
+		if (!identity) {
+			throw new ConvexError("Unauthorized");
+		}
+
+		const user = await ctx.db.get(args.userId);
+		if (!user) {
+			throw new ConvexError("User not found");
+		}
+
+		// check for the subscription
+		if (user.currentSubscriptionId) {
+			const subscription = await ctx.db.get(user.currentSubscriptionId);
+			if (subscription && subscription.status === "active") {
+				return { hasAccess: true, accessType: "subscription" };
+			}
+		}
+
+		// check for individual course access
+		const purchase = await ctx.db
+			.query("purchases")
+			.withIndex("by_userId_and_courseId", (q) => q.eq("userId", args.userId).eq("courseId", args.courseId))
+			.unique();
+
+		if (purchase) {
+			return { hasAccess: true, accessType: "course" };
+		}
+
+		return { hasAccess: false };
+
+
+
 	},
 });
